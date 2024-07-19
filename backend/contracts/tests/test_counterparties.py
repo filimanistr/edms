f1a17678a96502a848d59e78378c3e075c33a231
John <filimanistr@gmail.com>
2024-07-19 23:21:50 +0700

    write tests for counterparties views

--- backend/contracts/tests/test_counterparties.py
+++ backend/contracts/tests/test_counterparties.py
@@ -1,23 +1,33 @@
 from rest_framework.test import APITestCase
+from rest_framework import status
+
+from .tests import *
 
 
 class CounterpartyViewsTests(APITestCase):
 
+    @classmethod
+    def setUpTestData(cls):
+        cls.admin = create_test_admin()
+        cls.user = create_test_user("user1@test.com")
+        cls.another_user = create_test_user("user2@test.com")
+
     def test_user_cant_access_all_counterparties(self):
         """
-        Заказчик может получить информацию только о себе
+        Заказчик не может получить информацию о других пользователях
         """
-        pass
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get("/api/counterparties/")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_all_shown_to_admin(self):
         """
-        Отображаются все заказчики для исполнителя
+        Исполнитель может получить информацию о всех заказчиках
         """
-        pass
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.get("/api/counterparties/")
+        response_ids= [i["id"] for i in response.data]
+        ids = [self.user.counterparty.pk, self.another_user.counterparty.pk]
 
-
-    def test_can_only_edit_their_data(self):
-        """
-        Может менять только свою информацию
-        """
-        pass
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_ids, ids)
